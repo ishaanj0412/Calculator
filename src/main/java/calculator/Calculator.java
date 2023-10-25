df78dd125aef9c0c312381e18438d551f818fc80
ishaanj0412 <ishaanj0412@gmail.com>
2023-10-26 00:34:24 +0530

    updated structure

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+//import java.util.Scanner;
+//import java.util.logging.LogManager;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("-------------");
+        System.out.println("Calculator");
+        System.out.println("-------------");
+
+    }
+}
